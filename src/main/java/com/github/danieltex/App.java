bbb703acef869737576bdf823d63fd29b6396a5f
Daniel T. Santos <dtsantos@daitangroup.com>
2019-10-31 10:22:56 -0300

    PNG maze output

--- src/main/java/com/github/danieltex/App.java
+++ src/main/java/com/github/danieltex/App.java
@@ -1,5 +1,11 @@
 package com.github.danieltex;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 import com.github.danieltex.algorithms.BinaryTree;
 import com.github.danieltex.algorithms.SideWinder;
 import com.github.danieltex.grid.Grid;
@@ -10,14 +16,15 @@ import com.github.danieltex.grid.Grid;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        String algorithm = args[0].toUpperCase();
         int rows = Integer.parseInt(args[1]);
         int cols = Integer.parseInt(args[2]);
+        String filepath = args[3];
 
         Grid grid = new Grid(rows, cols);
 
-        switch(args[0].toUpperCase()) {
+        switch(algorithm) {
             case "SIDEWINDER":
                 System.out.println(SideWinder.on(grid));
                 break;
@@ -27,6 +34,15 @@ public class App
             default:
                 System.out.println("Unknow option: " + args[0]);
                 break;
+        }
+
+        BufferedImage img = grid.toPng(30);
+
+        try {
+            ImageIO.write(img, "png", new File(filepath));
+        } catch (IOException e) {
+            System.out.println("Failed to write file to " + filepath);
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/github/danieltex/grid/Grid.java
+++ src/main/java/com/github/danieltex/grid/Grid.java
@@ -8,7 +8,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public class Grid implements Iterable<Cell> {
+    public static final int DEFAULT_CELL_SIZE = 10;
+
     public final int rows;
     public final int columns;
 
@@ -21,6 +27,40 @@ public class Grid implements Iterable<Cell> {
         this.columns = columns;
         this.grid = prepareGrid();
         configureCells();
+    }
+
+    public BufferedImage toPng(int cellSize) {
+        final int imgWidth = cellSize * columns;
+        final int imgHeight = cellSize * rows;
+        
+        Color background = Color.WHITE;
+        Color wall = Color.BLACK;
+        
+        BufferedImage img = new BufferedImage(imgWidth + 1, imgHeight + 1, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = img.createGraphics();
+        g.setColor(background);
+        g.fillRect(0, 0, imgWidth+1, imgHeight+1);
+        g.setColor(wall);
+
+        for (Cell cell : allCells()) {
+            int x1 = cell.column * cellSize;
+            int y1 = cell.row * cellSize;
+            int x2 = (cell.column + 1) * cellSize;
+            int y2 = (cell.row + 1) * cellSize;
+
+            if (cell.north == null)         g.drawLine(x1, y1, x2, y1);
+            if (cell.west  == null)         g.drawLine(x1, y1, x1, y2);
+            if (!cell.isLinked(cell.east))  g.drawLine(x2, y1, x2, y2);
+            if (!cell.isLinked(cell.south)) g.drawLine(x1, y2, x2, y2);
+        }
+
+        g.dispose();
+        return img;
+    }
+
+
+    public BufferedImage toPng() {
+        return toPng(DEFAULT_CELL_SIZE);
     }
 
     @Override
